267ede24fa320c73d76fb5684e25562b2b417f52
Anteneh Anley <you@example.com>
2022-07-01 19:04:57 +0000

    print is done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - prints the phrase with proper gramma
- * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherw
+ * main - prints the phrase with proper grammar,
+ *  but the outcome is a piece of art,
+ *  Return: 0 if exited properly, non-zero otherwe
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
