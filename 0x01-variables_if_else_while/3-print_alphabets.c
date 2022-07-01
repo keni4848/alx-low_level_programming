6ee34a22d1a789e59ea65932cf9585603a7e476b
Anteneh Anley <you@example.com>
2022-07-01 21:42:49 +0000

    print alphabet projict done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+
 /**
-* main - prints the alphabet in lowercase, and th
+* main - prints the alphabet in lowercase, and then in uppercase,
 * followed by a new line
 * Return: Always 0 (Success)
 */
-main(void)
-int
+int main(void)
 {
 
 int ch;
+
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 for (ch = 'A'; ch <= 'Z'; ch++)
