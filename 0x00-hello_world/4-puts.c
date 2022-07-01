3b3002ca23cc4fdeacd0479b5daa3bc3c12fb48e
Anteneh Anley <you@example.com>
2022-07-01 13:50:31 +0000

    puts is done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main - prints the phrase "programming is like buliding a multiligual puzzle
+ *Retun: 0 if exited properly,non_zero othewise
+ */
+int main(void)
+{
+puts("\"programing is like building a multiilingual puzzle");
+retun(0);
+}
+
