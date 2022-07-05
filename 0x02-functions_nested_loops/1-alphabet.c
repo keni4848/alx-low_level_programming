9d5173c28cc2ccd4986388104f945e82fde5363a
Anteneh Anley <you@example.com>
2022-07-05 16:17:59 +0000

    void print alphabet is done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print alphabet- prints the alphabet, in lowercase,followed by a new line
+*/
+void printalphabet(void)
+{
+	char ch;
+
+	ch 'a';
+
+	while (ch <= 'z')
+{
+
+	_putchar(ch);
+	ch++;
+}
+	_putchar('\n');
+}
