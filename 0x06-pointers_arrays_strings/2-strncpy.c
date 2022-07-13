01e19c2d123e1b6c5eb7707c42dd0efeb172e907
Anteneh Anley <you@example.com>
2022-07-13 13:35:05 +0000

    strncpy is done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _strncpy - copies a string
+*_strncpy - copies a string
 * @dest: destination string
 * @src: source string
 * @n: number of bytes to copy
@@ -11,14 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 	i = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
+	while (src[i] != '\0' && i < n)
 {
 	dest[i] = src[i];
 	i++;
-	j++;
 }
+	while (i < n)
+{
 	dest[i] = '\0';
+	i++;
+}
 	return (dest);
 }
