b842cc24168ca0a12ac77cac7bc850a164d342d0
Anteneh Anley <you@example.com>
2022-07-14 02:27:46 +0000

    leet is done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,12 +7,12 @@
 */
 char *leet(char *s)
 {
-int i, j;
-char *a = "aAeEoOtTIL";
-char *b = "4433007711";
+	int i, j;
+	char *a = "aAeEoOtTIL";
+	char *b = "4433007711";
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 10; j++)
+	for (j = 0; j < 10; j++)
 {
 	if (s[i] == a[j])
 {
