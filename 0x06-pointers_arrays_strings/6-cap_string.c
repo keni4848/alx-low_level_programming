b4763d6e60de27dbe99e42c64cfb0994fee049bd
Anteneh Anley <you@example.com>
2022-07-14 01:49:52 +0000

    Write a function that capitalizes all words of a string cap done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,49 @@
 #include "main.h"
 /**
-* cap_string - capitalizes everey word of a string
-* @s: string to modify
-*
-* Return: the resulting string
-*/
-char *cap_string(char *s)
+ * *cap_string - capitalizes all words of a string
+ * @str: pointer to string
+ *
+ * Return: pointer to a capitalized string
+ */
+
+char *cap_string(char *str)
 {
 	int i, j;
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
-	'!', '?', '"', '(', ')', '{', '}'};
-for (i = 0; s[i] != '\0'; i++)
-{
-if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-	s[i] -= 32;
-for (j = 0; j < 13; j++)
-{
-if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-{
-	s[i + 1] -= 32;
+
+	char s[13];
+
+	s[0] = ',';
+	s[1] = ';';
+	s[2] = '!';
+	s[3] = '?';
+	s[4] = '"';
+	s[5] = '(';
+	s[6] = ')';
+	s[7] = '{';
+	s[8] = '}';
+	s[9] = '\t';
+	s[10] = '\n';
+	s[11] = ' ';
+	s[12] = '.';
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == s[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
 	}
-}
-}
-return (s);
+	return (str);
+
 }
